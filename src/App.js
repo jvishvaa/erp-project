12de9214ffabf577f7c87a9d5099a5840a27a168
Tony S Babu <tony@orchids.edu.in>
2020-10-18 20:04:23 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect } from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 function App() {
+  useEffect(() => {
+    store.dispatch({ type: 'SAMPLE_ACTION' });
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Router>
+        <Switch>
+          <Provider store={store}>
+            <Route path='/'>{() => <div>Landing page</div>}</Route>
+          </Provider>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/redux/store.js
@@ -0,0 +1,8 @@
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
+import logger from 'redux-logger';
+import rootReducer from './reducers';
+
+const middlewares = [logger, thunk];
+
+export default createStore(rootReducer, applyMiddleware(...middlewares));
