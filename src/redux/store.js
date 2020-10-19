d7aaa711dfa43c1796e080f313a61c59debf645f
Tony S Babu <tony@orchids.edu.in>
2020-10-19 12:24:56 +0530

    added redux dev tools extension

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,13 @@
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import logger from 'redux-logger';
 import rootReducer from './reducers';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
 const middlewares = [logger, thunk];
 
-export default createStore(rootReducer, applyMiddleware(...middlewares));
+export default createStore(
+  rootReducer,
+  composeEnhancers(applyMiddleware(...middlewares))
+);
