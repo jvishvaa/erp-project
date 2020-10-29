9b23d8d0bc6247dd7a8c0d0f204ac848fb705fb0
Subhradeep Mazumder <subhradeep@orchids.edu.in>
2020-10-29 10:37:57 +0530

    sending message and email

---
+++ src/container/communication/component/send-message/components/header-section.jsx
@@ -0,0 +1,51 @@
+/* eslint-disable react/no-array-index-key */
+/* eslint-disable no-debugger */
+/* eslint-disable no-console */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React from 'react';
+import './header-section.css';
+
+const HeaderSection = (props) => {
+  const { firstStep, secondStep, thirdStep, currentStep } = props || {};
+  return (
+    <div className='send_message_heading_wrapper'>
+      <div className='send_message_title'>Communication &gt; send sms/mail</div>
+      <div className='send_message_header'>
+        <div className='send_message_header_icon_wrapper'>
+          <div
+            className={`send_message_header_circle ${
+              firstStep ? 'send_message_header_border_circle' : null
+            } ${currentStep > 1 ? 'step_completed' : null}`}
+          >
+            1
+          </div>
+          <div className='send_message_header_title'>Get recepients</div>
+        </div>
+        <div className='send_message_header_bar' />
+        <div className='send_message_header_icon_wrapper'>
+          <div
+            className={`send_message_header_circle ${
+              secondStep ? 'send_message_header_border_circle' : null
+            } ${currentStep > 2 ? 'step_completed' : null}`}
+          >
+            2
+          </div>
+          <div className='send_message_header_title'>Select recepients</div>
+        </div>
+        <div className='send_message_header_bar' />
+        <div className='send_message_header_icon_wrapper'>
+          <div
+            className={`send_message_header_circle ${
+              thirdStep ? 'send_message_header_border_circle' : null
+            } ${currentStep > 3 ? 'step_completed' : null}`}
+          >
+            3
+          </div>
+          <div className='send_message_header_title'>Send message</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default HeaderSection;
