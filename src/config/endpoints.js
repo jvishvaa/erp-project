f0214c394e057892328849ca115e8a01ca865f8e
Hemanth <hemanth@orchids.edu.in>
2020-10-31 12:27:49 +0530

    adds: enpoints

--- src/config/endpoints.js
+++ src/config/endpoints.js
@@ -4,6 +4,9 @@ export default {
   },
   academics: {
     subjects: '/erp_user/subject/',
+    branches: '/erp_user/branch/',
+    grades: '/erp_user/grademapping/',
+    sections: '/erp_user/sectionmapping/',
   },
   communication: {
     roles: '/erp_user/roles/',
