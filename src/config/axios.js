5241a0d479161c0c626eeb7b307789e14d28e4fd
tony <tonysbabu@gmail.com>
2020-11-05 14:17:05 +0530

    api endpoint changed

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:8000',
+  baseURL: 'http://13.234.252.195:443',
 });
 
 // ****** enable the following code to attach authorization headers on each request and for logging out when an unauthorize request is send *****//
