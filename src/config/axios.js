2c9ceb64dbf80b6561154fb4319644cabe5c653f
tony <tonysbabu@gmail.com>
2020-11-06 17:55:27 +0530

    added bearer token to request

--- src/config/axios.js
+++ src/config/axios.js
@@ -6,28 +6,28 @@ const axiosInstance = axios.create({
 
 // ****** enable the following code to attach authorization headers on each request and for logging out when an unauthorize request is send *****//
 
-// axiosInstance.interceptors.request.use(async function (config) {
-//   const user = await localStorage.getItem('user');
-//   const requestConfig = config;
-//   if (user) {
-//     requestConfig.headers.Authorization = `JWT ${JSON.parse(user).token}`;
-//   }
-//   return config;
-// });
-// axiosInstance.interceptors.response.use(
-//   function (response) {
-//     return response;
-//   },
-//   async function (error) {
-// if (error.response?.status === 401) {
-//   console.log('user logged out');
-//   await localStorage.removeItem('user');
-//   // Show toast message login to continue
-// } else {
-//   // something went wrong
-// }
-// return Promise.reject(error);
-//   }
-// );
+axiosInstance.interceptors.request.use(async function (config) {
+  const user = await localStorage.getItem('userDetails');
+  const requestConfig = config;
+  if (user) {
+    requestConfig.headers.Authorization = `Bearer ${JSON.parse(user).token}`;
+  }
+  return config;
+});
+axiosInstance.interceptors.response.use(
+  function (response) {
+    return response;
+  },
+  async function (error) {
+    if (error.response?.status === 401) {
+      console.log('user logged out');
+      await localStorage.removeItem('userDetails');
+      // Show toast message login to continue
+    } else {
+      // something went wrong
+    }
+    return Promise.reject(error);
+  }
+);
 
 export default axiosInstance;
