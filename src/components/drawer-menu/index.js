d844ee24f8b8511ec84750355571baa1be43b4c8
tony <tonysbabu@gmail.com>
2020-11-17 15:31:18 +0530

    side drawer fixes

--- src/components/drawer-menu/index.js
+++ src/components/drawer-menu/index.js
@@ -1,62 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MenuItem from './menu-item';
+import SuperUserMenu from './super-user-menu';
 import './styles.scss';
 
-const DrawerMenu = ({ navigationItems, onClick }) => {
+const DrawerMenu = ({ navigationItems, superUser, onClick }) => {
+  const [openMenu, setOpenMenu] = useState(null);
   return (
     <>
+      {superUser && (
+        <SuperUserMenu
+          onClickMenuItem={onClick}
+          openMenu={openMenu}
+          onChangeMenuState={(menu) => {
+            if (menu === openMenu) {
+              setOpenMenu(null);
+            } else {
+              setOpenMenu(menu);
+            }
+          }}
+        />
+      )}
       {navigationItems &&
         navigationItems
           .filter((item) => item.child_module && item.child_module.length > 0)
-          .map((item) => <MenuItem item={item} onClick={onClick} />)}
-      {/* <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <DashboardIcon />
-        </ListItemIcon>
-        <ListItemText primary='Dashboard' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <ShoppingCartIcon />
-        </ListItemIcon>
-        <ListItemText primary='Orders' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <PeopleIcon />
-        </ListItemIcon>
-        <ListItemText primary='Customers' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <BarChartIcon />
-        </ListItemIcon>
-        <ListItemText primary='Reports' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <LayersIcon />
-        </ListItemIcon>
-        <ListItemText primary='Integrations' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary='Current month' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary='Last quarter' className={classes.menuItemText} />
-      </ListItem>
-      <ListItem button>
-        <ListItemIcon className={classes.menuItemIcon}>
-          <AssignmentIcon />
-        </ListItemIcon>
-        <ListItemText primary='Year-end sale' className={classes.menuItemText} />
-      </ListItem> */}
+          .map((item) => (
+            <MenuItem
+              item={item}
+              menuOpen={item.parent_modules === openMenu}
+              onChangeMenuState={() => {
+                if (item.parent_modules === openMenu) {
+                  setOpenMenu(null);
+                } else {
+                  setOpenMenu(item.parent_modules);
+                }
+              }}
+              onClick={onClick}
+            />
+          ))}
     </>
   );
 };
