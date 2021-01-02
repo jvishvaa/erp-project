71b842e0db947e9dd408820e540401dc09d4e282
Krushnakanta Mohakud <krushnakanta@orchids.edu.in>
2021-01-02 17:43:00 +0530

    minor

--- src/App.js
+++ src/App.js
@@ -38,7 +38,7 @@ import AddHomework from './containers/homework/teacher-homework/add-homework';
 import BulkUpload from './containers/user-management/bulk-upload/bulk-upload';
 import CoordinatorHomework from './containers/homework/coordinator-homework';
 import AddHomeworkCoord from './containers/homework/coordinator-homework/add-homework';
-
+import LessonPlan from './containers/lesson-plan/';
 
 const theme = createMuiTheme({
   palette: {
@@ -176,6 +176,9 @@ function App() {
                 <Route exact path='/homework/cadd/:date/:subject/:id/:coord_selected_teacher_id'>
                   {({ match }) => <AddHomeworkCoord match={match} />}
                 </Route>
+                <Route exact path='/lesson-plan'>
+                {({ match }) => <LessonPlan match={match} />}
+                 </Route>
 
 
               </Switch>
