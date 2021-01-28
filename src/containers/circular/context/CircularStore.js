efcaca4141098a6c622b33f8bab0f9faf392352d
Krushnakanta Mohakud <krushnakanta@orchids.edu.in>
2021-01-28 15:52:53 +0530

    minor-changes

--- src/containers/circular/context/CircularStore.js
+++ src/containers/circular/context/CircularStore.js
@@ -1,11 +1,5 @@
 import React, { createContext, useState } from 'react'
 
-
-// const initialState={
-//     a:'kkm',
-//     b:'mkk',
-// }
-
 const initialState={
     editData:[],
     isEdit: false,
