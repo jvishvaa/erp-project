58638141009209c9f9a258d27de0565bf32853c1
Nitesh Rathod <nitesh.rathod@orchids.edu.in>
2021-02-01 16:40:25 +0530

    aol class view merged

---
+++ src/containers/online-class/aol-view/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../Layout';
+import UpcomingClasses from './UpcomingClasses';
+
+const AOLClassView = () => {
+  return (
+    <div>
+      <Layout>
+          <UpcomingClasses />
+      </Layout>
+    </div>
+  );
+};
+
+export default AOLClassView;
