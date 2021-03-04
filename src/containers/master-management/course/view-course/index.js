7a90534edcdd54c559db8b40373c7aeab7ef7f12
nitesh.rathod@orchids.edu.in <nitesh.rathod@orchids.edu.in>
2021-03-04 21:54:25 +0530

    course list bugs fixes

--- src/containers/master-management/course/view-course/index.js
+++ src/containers/master-management/course/view-course/index.js
@@ -64,7 +64,7 @@ const CourseView = () => {
     if (gradeIds.length !== 0 && (tabMenuval === 0 || tabMenuval === undefined || tabMenuval === null)) {
       axiosInstance
         .get(
-          `${endpoints.onlineCourses.courseList}?grade=${gradeIds}&page=${page}&page_size=${limit}&all`
+          `${endpoints.onlineCourses.courseList}?grade=${gradeIds}&page=${page}&page_size=${limit}&all&is_delete=false`
         )
         .then((result) => {
           if (result.data.status_code === 200) {
