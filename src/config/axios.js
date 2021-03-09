f71b87c2f841acceab97cf8e7035a23eb3db2b0c
Nitin Kewlani <nitin.kewlani@orchids.edu.in>
2021-03-09 14:55:41 +0530

    hw admin bug fix

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  // baseURL: `${window.location.origin}/qbox`,
+  baseURL: `${window.location.origin}/qbox`,
   // baseURL: 'https://erpnew.letseduvate.com/qbox',
-  baseURL: 'https://revamperp.letseduvate.com/qbox',
+  // baseURL: 'https://revamperp.letseduvate.com/qbox',
   // baseURL: 'https://aol.letseduvate.com/qbox',
   // baseURL: 'http://13.234.252.195:443',
   // baseURL: 'http://localhost:8000/qbox',
