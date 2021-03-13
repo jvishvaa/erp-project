e64dca6eca99c35627306b4d38f6333f95909691
krushnakanta <krushnakanta@orchids.edu.in>
2021-03-13 15:02:28 +0530

    erp-online-class-multi-task-done

--- src/config/axios.js
+++ src/config/axios.js
@@ -7,7 +7,8 @@ const axiosInstance = axios.create({
   // baseURL: 'https://aol.letseduvate.com/qbox',
   // baseURL: 'http://olvorchidnaigaon.letseduvate.com/qbox',
   // baseURL: 'http://13.234.252.195:443',
-  baseURL: 'http://localhost:8000/qbox',
+  baseURL: 'http://192.168.0.96:8000/qbox',
+  // baseURL:'http://192.168.0.96/qbox'
 });
 
 // ****** enable the following code to attach authorization headers on each request and for logging out when an unauthorize request is send *****//
