56de0d626f6ff1888ab58da9e9848a5c375382f7
krushnakanta <krushnakanta@orchids.edu.in>
2021-03-15 15:56:32 +0530

    last:nothing-added

--- src/config/axios.js
+++ src/config/axios.js
@@ -2,12 +2,12 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   //  baseURL: `${window.location.origin}/qbox`,
-  baseURL:'https://revamperp.letseduvate.com/qbox',
+  // baseURL:'https://revamperp.letseduvate.com/qbox',
   // baseURL: 'https://erpnew.letseduvate.com/qbox',
   // baseURL: 'https://aol.letseduvate.com/qbox',
   // baseURL: 'http://olvorchidnaigaon.letseduvate.com/qbox',
   // baseURL: 'http://13.234.252.195:443',
-  // baseURL: 'http://192.168.0.96:8000/qbox',
+  baseURL: 'http://192.168.0.96:8000/qbox',
   // baseURL:'http://192.168.0.96/qbox'
 });
 
