3903cd17a77edac6c5c924646aaa70254859436e
manikanta3 <manikanta@orchids.edu.in>
2021-03-18 18:48:21 +0530

    Mp quiz: progress commit

--- src/components/mp-quiz/mp-quiz-providers/mp-quiz-constants/urls.js
+++ src/components/mp-quiz/mp-quiz-providers/mp-quiz-constants/urls.js
@@ -4,16 +4,27 @@ const userAuthToken =
   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozNjcxLCJ1c2VybmFtZSI6IjIwMDEyMzAwMDUiLCJleHAiOjY2MTU1Mjg1MzIsImVtYWlsIjoia3V2aWthc2gxMjNAZ21haWwuY29tIn0.yw3hZZ5GwrnDRrjGdLhmFm5v2QlA8HQ0yAHc7NQw8Jo';
 
 const ajaxBaseURL = axiosInstance.defaults.baseURL; // 'http://127.0.0.1:8000/qbox';
-const ajaxHeaders = {
-  headers: {
-    Authorization: `Bearer ${userAuthToken}`,
-    // 'x-api-key': 'vikash@12345#1231',
-  },
-};
-const urls = {
-  socketBase: 'ws://localhost:8000/ws',
-  socketEndPoint: '/multiplayer-quiz/<online_class_id>/<user_auth_token>/',
 
+const ajaxHeaders = { headers: { Authorization: `Bearer ${userAuthToken}` } };
+const ajaxHeadersForCentral = { headers: { 'x-api-key': 'vikash@12345#1231' } };
+
+const genSocketBase = () => {
+  const { port: isLocal, host } = new URL(ajaxBaseURL);
+  const protocol = isLocal ? 'ws' : 'wss';
+  // const port = isLocal?isLocal:'443'
+  return `${protocol}://${host}`;
+};
+const socketBase = genSocketBase();
+const socketBaseURL = `${socketBase}/ws`;
+const urls = {
+  // socketBase: 'ws://localhost:8000/ws',
+
+  socketEndPoint: '/multiplayer-quiz/<online_class_id>/<user_auth_token>/',
+  quizSocketURL: {
+    baseURL: socketBaseURL,
+    headers: {},
+    endpoint: `${socketBaseURL}/multiplayer-quiz/<online_class_id>/<user_auth_token>/`,
+  },
   fetchQuizBgms: {
     baseURL: ajaxBaseURL,
     headers: ajaxHeaders,
@@ -27,7 +38,10 @@ const urls = {
 
   ajaxHeaders,
   ajaxBaseURL,
+  ajaxHeadersForCentral,
   userAuthToken,
+  socketBase,
+  socketBaseURL,
 };
 
 export default urls;
--- src/components/mp-quiz/mp-quiz-ui/index.js
+++ src/components/mp-quiz/mp-quiz-ui/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { constants, QuizContextHome } from '../mp-quiz-providers';
 import MpQuiz from './mp-quiz';
 
-const { urls } = constants;
-// const userAuthToken =
-//   'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyX2lkIjozNjcxLCJ1c2VybmFtZSI6IjIwMDEyMzAwMDUiLCJleHAiOjY2MTU1Mjg1MzIsImVtYWlsIjoia3V2aWthc2gxMjNAZ21haWwuY29tIn0.yw3hZZ5GwrnDRrjGdLhmFm5v2QlA8HQ0yAHc7NQw8Jo';
+const {
+  urls: { quizSocketURL: { endpoint: quizSocketURLEndpoint } = {} } = {},
+} = constants;
 
 const { token: userAuthToken } =
   JSON.parse(localStorage.getItem('userDetails') || JSON.stringify({})) || {};
@@ -12,7 +12,7 @@ function MpQuizHome(props) {
   const { lobbyIdentifier: lobbyIdentifierFProps = 907 } = props || {};
   const [lobbyIdentifier] = React.useState(lobbyIdentifierFProps);
 
-  const socketUrl = (urls.socketBase + urls.socketEndPoint)
+  const socketUrl = quizSocketURLEndpoint
     .replace('<online_class_id>', lobbyIdentifier)
     .replace('<user_auth_token>', userAuthToken);
 
