c11321a83d2ba97b7abe195b78b3555f08a35165
Rajneesh Singh <rajneesh.singh@orchids.edu.in>
2021-03-26 13:52:39 +0530

    added session in section api

--- src/config/axios.js
+++ src/config/axios.js
@@ -11,7 +11,7 @@ const axiosInstance = axios.create({
   // baseURL:'http://192.168.0.96/qbox'
   // baseURL: 'http://erp.finance.letseduvate.com/qbox',
   // baseURL: 'http://dev.olvorchidnaigaon.letseduvate.com/qbox',
-  // baseURL: 'http://localhost:8000/qbox'
+  baseURL: 'http://localhost:8000/qbox'
   // baseURL: 'http://192.168.0.102:8000/qbox'
 });
 
