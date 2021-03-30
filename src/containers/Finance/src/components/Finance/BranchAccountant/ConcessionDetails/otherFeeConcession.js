b02b8fc6c890df94163e5edae14000ed117c840d
Rajneesh Singh <rajneesh.singh@orchids.edu.in>
2021-03-30 12:27:16 +0530

    fix module id and branch issue

--- src/containers/Finance/src/components/Finance/BranchAccountant/ConcessionDetails/otherFeeConcession.js
+++ src/containers/Finance/src/components/Finance/BranchAccountant/ConcessionDetails/otherFeeConcession.js
@@ -25,7 +25,7 @@ const OtherFeeConcession = ({
   useEffect(() => {
     if (getData && erp.length >= 10 && session) {
       console.log('Fee Concession')
-      fetchListConcession(session, erp, alert, user)
+      fetchListConcession(session, erp, alert, user, this.props.branchId, this.props.moduleId)
       // setValue(true)
     } else {
       alert.warning('Select Required Fields')
@@ -143,7 +143,7 @@ const mapStateToProps = state => ({
 })
 
 const mapDispatchToProps = dispatch => ({
-  fetchListConcession: (session, erp, alert, user) => dispatch(actionTypes.fetchOtherFeeConcessionList({ session, erp, alert, user }))
+  fetchListConcession: (session, erp, alert, user, branch, modoleId) => dispatch(actionTypes.fetchOtherFeeConcessionList({ session, erp, alert, user, branch, modoleId }))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(OtherFeeConcession)
