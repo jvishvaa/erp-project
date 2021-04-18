2219cc830f641efb85c50cb9b842e1bfefaf1b0f
krushnakanta <krushnakanta@orchids.edu.in>
2021-04-18 18:37:57 +0530

    minor-fix

--- src/config/config.js
+++ src/config/config.js
@@ -3,8 +3,8 @@ const local = {
     BUCKET: 'https://omrsheet.s3.ap-south-1.amazonaws.com',
   },
   apiGateway: {
-    // baseURL: 'https://orchids.letseduvate.com/qbox',
-    baseURL: `http://dev.olvorchidnaigaon.letseduvate.com/qbox`,
+    baseURL: 'https://orchids.letseduvate.com/qbox',
+    // baseURL: `http://dev.olvorchidnaigaon.letseduvate.com/qbox`,
     baseURLCentral: 'https://dev.mgmt.letseduvate.com/qbox',
     xAPIKey: 'vikash@12345#1231',
   },
@@ -25,7 +25,7 @@ const olv = {
     BUCKET: 'https://omrsheet.s3.ap-south-1.amazonaws.com',
   },
   apiGateway: {
-    baseURL: `https://dev.olvorchidnaigaon.letseduvate.com/qbox`,
+    baseURL: `https://olvorchidnaigaon.letseduvate.com/qbox`,
     baseURLCentral: 'https://dev.mgmt.letseduvate.com/qbox',
     xAPIKey: 'vikash@12345#1231',
   },
