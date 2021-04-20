a997961a2a795766a60e3867f09f709c1bbb23f9
Abhishek Manglam <abhishek.k@orchids.edu.in>
2021-04-20 11:34:12 +0530

    Contact UI

--- src/containers/contact-us/index.jsx
+++ src/containers/contact-us/index.jsx
@@ -107,6 +107,9 @@ const ContactUs = () => {
   const [selectedAcademicYear, setSelectedAcadmeicYear] = useState('');
   const [branchList, setBranchList] = useState([])
   const [selectedBranch, setSelectedBranch] = useState([])
+  const [num,setNum]=useState('')
+  const [num1,setNum1]=useState('')
+  const [num2,setNum2]=useState('')
   const moduleId = 175;
 
   const [value, setValue] = React.useState(0);
@@ -121,7 +124,24 @@ const ContactUs = () => {
     }
   }, [moduleId]);
 
-
+  const handleNumber=(e)=>{
+      const re=/^[0-9]+$/g
+      if((e.target.value==="" || re.test(e.target.value))&& (e.target.value.length<=10)){
+          setNum(e.target.value)
+      }
+  }
+  const handleNumber1=(e)=>{
+    const re=/^[0-9]+$/g
+    if((e.target.value==="" || re.test(e.target.value))&& (e.target.value.length<=10)){
+        setNum1(e.target.value)
+    }
+}
+const handleNumber2=(e)=>{
+  const re=/^[0-9]+$/g
+  if((e.target.value==="" || re.test(e.target.value))&& (e.target.value.length<=10)){
+      setNum2(e.target.value)
+  }
+}
   function callApi(api, key) {
     setLoading(true);
     axiosInstance.get(api)
@@ -235,13 +255,13 @@ const ContactUs = () => {
           />
         </Grid>
         <form className={classes.root} noValidate autoComplete="off">
-            <TextField id="foe-contact-number" label="FOE Contact Number" fullWidth/>
+            <TextField id="foe-contact-number" label="FOE Contact Number" type="text" fullWidth  value={num||""} onChange={(e)=>handleNumber(e)}/>
             <br />
             <br />
-            <TextField id="operational-manager-number" label="Operational Manager Contanct Number" fullWidth/>
+            <TextField id="operational-manager-number" label="Operational Manager Contact Number" type="text" value={num1||""} onChange={(e)=>handleNumber1(e)} fullWidth/>
             <br />
             <br />
-            <TextField id="campus-incharge-number" label="Campus Incharge Contact Number" fullWidth/>
+            <TextField id="campus-incharge-number" label="Campus Incharge Contact Number" type="text" value={num2||""} fullWidth onChange={(e)=>handleNumber2(e)}/>
             <br />
             <br />
             <Button variant="contained" color="primary">
@@ -251,10 +271,98 @@ const ContactUs = () => {
         </Grid>
       </TabPanel>
       <TabPanel value={value} index={1}>
-        UPDATE CONTACT NUMBER
+      <Grid container direction="row" spacing={3} className={classes.root} alignItems="center" alignContent="center">
+        <Grid item md={4} xs={12} >
+          <Autocomplete
+            size='small'
+            fullWidth
+            style={{width:'100%'}}
+            onChange={(event, value) => {
+              setSelectedAcadmeicYear(value)
+              console.log(value, "test")
+              if (value) {
+                callApi(
+                  `${endpoints.communication.branches}?session_year=${value?.id}&module_id=${moduleId}`,
+                  'branchList'
+                );
+              }
+              setSelectedBranch([])
+
+            }}
+            id='branch_id'
+            className='dropdownIcon'
+            value={selectedAcademicYear || ""}
+            options={academicYear || ""}
+            getOptionLabel={(option) => option?.session_year || ""}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant='outlined'
+                label='Academic Year'
+                placeholder='Academic Year'
+              />
+            )}
+          />
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <Autocomplete
+            // multiple
+            fullWidth
+            size='small'
+            style={{width:'100%'}}
+            onChange={(event, value) => {
+              setSelectedBranch([])
+              if (value) {
+                // const ids = value.map((el)=>el)
+                const selectedId = value.branch.id
+                setSelectedBranch(value)
+                console.log(value)
+                callApi(
+                  `${endpoints.academics.grades}?session_year=${selectedAcademicYear.id}&branch_id=${selectedId.toString()}&module_id=${moduleId}`,
+                  'gradeList'
+                );
+              }
+            }}
+            id='branch_id'
+            className='dropdownIcon'
+            value={selectedBranch || ""}
+            options={branchList || ""}
+            getOptionLabel={(option) => option?.branch?.branch_name || ""}
+            filterSelectedOptions
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                variant='outlined'
+                label='Branch'
+                placeholder='Branch'
+              />
+            )}
+          />
+        </Grid>
+        <Grid item xs={12} md={4} alignItems="center" alignContent="center" justify="center">
+        <Button variant="contained" color="primary">
+                Filter
+            </Button>
+        </Grid>
+        <form className={classes.root} noValidate autoComplete="off">
+            <TextField id="foe-contact-number" label="FOE Contact Number" type="text" fullWidth  value={num||""} onChange={(e)=>handleNumber(e)}/>
+            <br />
+            <br />
+            <TextField id="operational-manager-number" label="Operational Manager Contact Number" type="text" value={num1||""} onChange={(e)=>handleNumber1(e)} fullWidth/>
+            <br />
+            <br />
+            <TextField id="campus-incharge-number" label="Campus Incharge Contact Number" type="text" value={num2||""} fullWidth onChange={(e)=>handleNumber2(e)}/>
+            <br />
+            <br />
+            <Button variant="contained" color="primary">
+                Send
+            </Button>
+        </form>
+        </Grid>
       </TabPanel>
           </Grid>
-        {loading && <Loader />}
+        {/* {loading && <Loader />} */}
     </Layout>
   );
 };
