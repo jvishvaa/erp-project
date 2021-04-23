f81f7cacd18110c562a194556795f9c2d23d2c1f
Rajneesh Singh <rajneesh.singh@orchids.edu.in>
2021-04-23 16:52:25 +0530

    Chnage in base url

--- src/containers/Finance/src/urls.js
+++ src/containers/Finance/src/urls.js
@@ -103,9 +103,9 @@ if (process.env.REACT_APP_UI_ENV === 'dev_aol') {
 } else {
   HOSTNAME = 'localhost'
   PORT = '8000'
-    //  BASE =`${window.location.origin}/qbox`
+     BASE =`${window.location.origin}/qbox`
   // BASE = 'http://erp.finance.letseduvate.com' + '/qbox'
-  BASE = 'http://localhost:8000' + '/qbox'
+  // BASE = 'http://localhost:8000' + '/qbox'
   FRONTENT_BASE = PROTO + '://' + HOSTNAME + ':' + '3000'
   BASE_URL = PROTO + '://' + HOSTNAME + ':8000' + '/qbox'
   MEDIA_BASE = 'http://localhost:8000'
