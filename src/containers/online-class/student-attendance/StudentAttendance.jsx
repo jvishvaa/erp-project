9695ca8f921bc4dc91a7dd248e2e5b03a4b88fe7
vinodkumar10 <vinodkumar@orchids.edu.in>
2021-05-11 09:08:47 +0530

    table added in student attendance report module

--- src/containers/online-class/student-attendance/StudentAttendance.jsx
+++ src/containers/online-class/student-attendance/StudentAttendance.jsx
@@ -1,5 +1,18 @@
 import React, { useState } from 'react';
-import { Button, Grid, TextField } from '@material-ui/core';
+import {
+  Button,
+  Grid,
+  TextField,
+  Divider,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  SvgIcon,
+  Switch,
+} from '@material-ui/core';
 import CommonBreadcrumbs from 'components/common-breadcrumbs/breadcrumbs';
 import Layout from 'containers/Layout';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
@@ -7,6 +20,10 @@ import { Autocomplete } from '@material-ui/lab';
 import { LocalizationProvider, DateRangePicker } from '@material-ui/pickers-4.2';
 import MomentUtils from '@material-ui/pickers-4.2/adapter/moment';
 import moment from 'moment';
+import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
+import RemoveCircleIcon from '@material-ui/icons/RemoveCircle';
+import unfiltered from '../../../assets/images/unfiltered.svg';
+import selectfilter from '../../../assets/images/selectfilter.svg';
 
 function StudentAttendance({ history }) {
   const [classTypes, setClassTypes] = useState([
@@ -23,7 +40,14 @@ function StudentAttendance({ history }) {
 
   const [startDate, setStartDate] = useState('');
   const [endDate, setEndDate] = useState('');
-
+  const [isHidden, setIsHidden] = useState(window.innerWidth < 600);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isEdit, setIsEdit] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const toggleHide = () => {
+    setIsHidden(!isHidden);
+  };
+  const [attendeeList, setAttendeeList] = useState([]);
   function handleDate(v1) {
     if (v1 && v1.length !== 0) {
       setStartDate(moment(new Date(v1[0])).format('YYYY-MM-DD'));
@@ -33,6 +57,17 @@ function StudentAttendance({ history }) {
     }
     // setDateRangeTechPer(v1);
   }
+  const handleGetAttendance = () => {
+    const payload = {
+      class_type: selectedClassType,
+      start_date: startDate,
+      end_date: endDate,
+    };
+    console.log(payload, 'checking data');
+  };
+  const handleClearAll = () => {
+    setSelectedClassType([]);
+  };
   return (
     <>
       <Layout>
@@ -113,27 +148,115 @@ function StudentAttendance({ history }) {
                   />
                 </LocalizationProvider>
               </Grid>
-              <Grid item md={2} xs={12}>
+              <Grid item md={2} xs={12} direction='row' justify='center'>
                 <Button
                   variant='contained'
                   size='large'
                   className='BatchViewfilterButtons'
+                  onClick={handleGetAttendance}
                 >
                   Get Attendance
                 </Button>
               </Grid>
               {/* <Grid container spacing={2} style={{ marginTop: '5px' }}> */}
-              <Grid item md={2} xs={12} style={{ marginLeft: '-20px' }}>
+              <Grid
+                item
+                md={2}
+                xs={12}
+                style={{ marginLeft: '-20px' }}
+                direction='row'
+                justify='center'
+              >
                 <Button
                   variant='contained'
                   size='large'
                   className='BatchViewfilterButtons'
+                  onClick={handleClearAll}
                 >
                   Clear All
                 </Button>
               </Grid>
               {/* </Grid> */}
             </Grid>
+          </Grid>
+          <Divider style={{ margin: '10px 0px' }} />
+          <Grid
+            itemxs={12}
+            className='attendee__management-table'
+            style={{ width: '100%' }}
+          >
+            {isHidden ? (
+              <AddCircleOutlineIcon className='expand-management' onClick={toggleHide} />
+            ) : (
+              <RemoveCircleIcon className='expand-management' onClick={toggleHide} />
+            )}
+            <TableContainer>
+              <Table className='viewclass__table' aria-label='simple table'>
+                <TableHead className='styled__table-head'>
+                  <TableRow>
+                    <TableCell align='center' className={`${isHidden ? 'hide' : 'show'}`}>
+                      SL_NO.
+                    </TableCell>
+                    <TableCell align='center'>Student name</TableCell>
+                    <TableCell align='center'>Erp</TableCell>
+                    <TableCell align='center'>Attended status</TableCell>
+                  </TableRow>
+                </TableHead>
+                {attendeeList && attendeeList.length > 0 ? (
+                  <TableBody className='styled__table-body'>
+                    {attendeeList.map((el, index) => {
+                      return (
+                        <TableRow key={el.id}>
+                          <TableCell
+                            align='center'
+                            className={`${isHidden ? 'hide' : 'show'}`}
+                          >
+                            {index + currentPage * 10 - 9}
+                          </TableCell>
+                          <TableCell align='center'>vinod</TableCell>
+                          {/* <TableCell align='center'>{el.user.user.first_name}</TableCell> */}
+                          <TableCell align='center'>vinod</TableCell>
+                          {/* <TableCell align='center'>{el.user.user.username}</TableCell> */}
+                          <TableCell align='center'>
+                            'Attended
+                            {''}
+                          </TableCell>
+                        </TableRow>
+                      );
+                    })}
+                  </TableBody>
+                ) : (
+                  <div className='attendanceDataUnavailable'>
+                    <SvgIcon
+                      component={() => (
+                        <img
+                          style={
+                            // isMobile
+                            //   ? { height: '100px', width: '200px' }
+                            // :
+                            { height: '160px', width: '290px' }
+                          }
+                          src={unfiltered}
+                        />
+                      )}
+                    />
+                    <SvgIcon
+                      component={() => (
+                        <img
+                          style={
+                            // isMobile
+                            //   ? { height: '20px', width: '250px' }
+                            //   :
+                            { height: '50px', width: '400px', marginLeft: '5%' }
+                          }
+                          src={selectfilter}
+                        />
+                      )}
+                    />
+                  </div>
+                )}
+              </Table>
+            </TableContainer>
           </Grid>
         </Grid>
       </Layout>
