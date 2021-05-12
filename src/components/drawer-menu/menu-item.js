0773a6b2c102e7e613337ee0ea3f82c5d89aa2de
vinodkumar10 <vinodkumar@orchids.edu.in>
2021-05-12 15:55:53 +0530

    resolved the confilts

--- src/components/drawer-menu/menu-item.js
+++ src/components/drawer-menu/menu-item.js
@@ -87,7 +87,7 @@ const MenuItem = withRouter(({ history, ...props }) => {
     { name: 'Question Paper', Path: '/assessment-question' },
     { name: 'Create Test', Path: '/assesment' },
     { name: 'Take Test', Path: '/assessment' },
-    {name: 'Student Attendance Report', path: '/student-attendance-report'}
+    { name: 'Student Attendance Report', Path: '/student-attendance-report' },
     { name: 'Student Strength', Path: '/student-strength' },
   ];
   // const [menuOpen, setMenuOpen] = useState(false);
