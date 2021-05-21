07862f81fbc3b36b187ac1601e5dbdc8d50b59d3
aamani <aamanilakshmi@gmail.com>
2021-05-21 14:13:18 +0530

    replaced missing components

--- src/containers/ebooks/filter.jsx
+++ src/containers/ebooks/filter.jsx
@@ -68,7 +68,7 @@ const Filter = ({ handleFilter, clearFilter }) => {
         } else if (key === 'grade') {
           setGradeList(response.data.result);
         } else if (key === 'subject') {
-          setSubjectList(response.data.result[0].subject_id_name);
+          setSubjectList(response.data.result);
         }
       }
       }).catch(error => {
@@ -195,8 +195,9 @@ const Filter = ({ handleFilter, clearFilter }) => {
             style={{ width: '100%' }}
             id='subject'
             options={subjectList}
+            getOptionLabel={(option) => option && option.subject_id_name&& option.subject_id_name[0] &&option.subject_id_name[0].erp_sub_name||''}
             value={selectedSubject}
-            getOptionLabel={(option) => option?.erp_sub_name||''}
+            // getOptionLabel={(option) => option?.erp_sub_name||''}
             filterSelectedOptions
             renderInput={(params) => (
               <TextField
