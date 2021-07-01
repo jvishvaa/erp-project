6a699b4cb33ea783692169f9b76190c2a2513b57
Nitin Kewlani <nitin.kewlani@orchids.edu.in>
2021-07-01 19:28:04 +0530

    top performer api calls reduced

--- src/containers/homework/student-homework/components/top-performer-card/top-performer-card.jsx
+++ src/containers/homework/student-homework/components/top-performer-card/top-performer-card.jsx
@@ -34,22 +34,50 @@ const TopPerformerCard = withRouter(({ history, ...props }) => {
   const [showTopPerformer, setShowTopPerformer] = useState(false);
   const [showTopPerformerIndex, setShowTopPerformerIndex] = useState();
 
+  useEffect(() => {
+    localStorage.removeItem('topperformer');
+  }, [])
+
+  const getFromLocalStorage = (id) => {
+    const topPerformerData = JSON.parse(localStorage.getItem(`topperformer`)) || [];
+    let response = '';
+    topPerformerData.forEach((element) => {
+      if (element[id]) response = element[id];
+    })
+    return response;
+  }
+
+  const setInLocalStorage = (data = [], id) => {
+    const topPerformerData = JSON.parse(localStorage.getItem(`topperformer`)) || [];
+    const newTopPerformer = {};
+    newTopPerformer[id] = data || [];
+    localStorage.setItem('topperformer', JSON.stringify([...topPerformerData, newTopPerformer]))
+  }
+
   const handleTopPerformer = async (index, id) => {
-    try {
-      const result = await axiosInstance.get(
-        `${endpoints.homeworkStudent.getTopPerformer}?subject_id=${id}`
-      );
-      if (result.data.status_code === 200) {
-        setTopPerformer(result.data.data);
-        setShowTopPerformer(true);
-        setShowTopPerformerIndex(index);
-      } else {
-        setShowTopPerformer(true);
-        setShowTopPerformerIndex(index);
-        setAlert('error', result.data.message);
+    const storedData = getFromLocalStorage(id) || '';
+    if (storedData) {
+      setTopPerformer(storedData);
+      setShowTopPerformer(true);
+      setShowTopPerformerIndex(index);
+    } else {
+      try {
+        const result = await axiosInstance.get(
+          `${endpoints.homeworkStudent.getTopPerformer}?subject_id=${id}`
+        );
+        if (result?.data?.status_code === 200) {
+          setInLocalStorage(result?.data?.data, id);
+          setTopPerformer(result?.data?.data);
+          setShowTopPerformer(true);
+          setShowTopPerformerIndex(index);
+        } else {
+          setShowTopPerformer(true);
+          setShowTopPerformerIndex(index);
+          setAlert('error', result?.data?.message);
+        }
+      } catch (error) {
+        setAlert('error', error?.message);
       }
-    } catch (error) {
-      setAlert('error', error.message);
     }
   };
 
@@ -70,7 +98,7 @@ const TopPerformerCard = withRouter(({ history, ...props }) => {
             variant='body2'
             className='homework_timeline_card_info top-performer-row'
             component='p'
-            key={`top-performer-row${index}`}
+            key={`top - performer - row${index}`}
           >
             <span className='top-performer-row-index'>{index + 1}.</span>
             <span className='subject_rating_first_letter'>
@@ -81,8 +109,8 @@ const TopPerformerCard = withRouter(({ history, ...props }) => {
             </span>
             <span
               className='top_performer_icon'
-              // onClick={() => handleTopPerformer(index, subject.id)}
-              // onMouseLeave={handleTopPerformerRemove}
+              onClick={() => handleTopPerformer(index, subject.id)}
+              onMouseLeave={handleTopPerformerRemove}
             >
               <SvgIcon
                 component={() => (
@@ -97,8 +125,8 @@ const TopPerformerCard = withRouter(({ history, ...props }) => {
                 )}
               />
               {(showTopPerformerIndex === index &&
-              showTopPerformer &&
-              topPerformer.length>=0) ? (
+                showTopPerformer &&
+                topPerformer.length >= 0) ? (
                 <span className='tooltiptext'>
                   {topPerformer.map((student, index) => (
                     <div
@@ -108,7 +136,7 @@ const TopPerformerCard = withRouter(({ history, ...props }) => {
                       {index + 1}. {`${student.first_name} ${student.last_name}`}
                     </div>
                   ))}
-                  {topPerformer.length<=0 && <div className='student_details_info'>No Record Available!</div>}
+                  {topPerformer.length <= 0 && <div className='student_details_info'>No Record Available!</div>}
                 </span>
               ) : null}
             </span>
