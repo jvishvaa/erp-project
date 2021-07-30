04ac603a5751b686b9ae489454ec6bc614ef7532
MamataNayak <mamata.nayak@orchids.edu.in>
2021-07-30 18:16:54 +0530

    MPQuiz bug fixes-qa

--- src/App.js
+++ src/App.js
@@ -618,7 +618,7 @@ function App({ alert }) {
                           <Route exact path='/erp-online-class/assign/:id/qp'>
                             {({ match }) => <AssignQP match={match} />}
                           </Route>
-                          <Route exact path='/erp-online-class/:id/pre-quiz'>
+                          <Route exact path='/erp-online-class/:id/:qid/pre-quiz'>
                             {({ match }) => <PreQuiz match={match} />}
                           </Route>
                           <Route path='/erp-online-class/:onlineclassId/quiz/:questionpaperId/:lobbyuuid/:role'>
