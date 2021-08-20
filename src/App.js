7f8a4df571be84ad101110e41b2142a13b7655b0
karthik_k12 <v.karthik@orchids.edu.in>
2021-08-20 13:16:55 +0530

    online class ms api call oly at intial

--- src/App.js
+++ src/App.js
@@ -293,8 +293,12 @@ import ViewiBook from './containers/intelligent-textbook/ViewiBook';
 import AllBooksPage from 'containers/intelligent-textbook/bookpage/AllBooksPage';
 import ChapterBook from 'containers/intelligent-textbook/chapterpage/ChapterBook';
 import { themeGenerator } from '../src/utility-functions/themeGenerator';
+import StoreAtStudent from 'containers/Finance/src/components/Inventory/Student/storeAtStudent';
+import  EditStoreTransactionDetails from 'containers/Finance/src/components/Finance/StorePaymentRequests/editStoreTransactionDetails';
+import { isMsAPI } from "./utility-functions/index";
 
 function App({ alert }) {
+  isMsAPI();
   const [theme, setTheme] = useState(() => themeGenerator());
   return (
     // <ErrorBoundary404 HomeButton={false}>
