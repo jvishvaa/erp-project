582b6e61abb2def81d0387949941a79236a3d014
Nitin Kewlani <nitin.kewlani@orchids.edu.in>
2021-12-01 07:34:05 +0000

    Merge branch 'chander-frontend' into 'dev'
    Chander frontend
    
    See merge request k12devs1/erp-revamp-frontend!3192
    
    (cherry picked from commit 931a4623f56f846a03956744ae1860459c7d082e)
    
    3071f5f8 resolved conflict
    bc9670be resolved local coonflict 2
    db7756c0 Merge branch 'dev' of gitlab.com:k12devs1/erp-revamp-frontend into chander-frontend
    91af99bd Merge branch 'dev' of gitlab.com:k12devs1/erp-revamp-frontend into chander-frontend
    c2e19a8a HomeWork Upload Popup is vanishing if i Click Anywhere in UI

--- src/containers/online-class/erp-view-class/admin/UploadDialogBox.js
+++ src/containers/online-class/erp-view-class/admin/UploadDialogBox.js
@@ -298,6 +298,7 @@ const UploadClassWorkDiaogBox = (props) => {
         style={{ zIndex: '3' }}
         onClose={handleClose}
         aria-labelledby='form-dialog-title'
+        disableBackdropClick
       >
         <DialogTitle className={classes.uploadBoxTitle} id='form-dialog-title'>
           {isTeacher ? 'Classwork' : 'Upload Classwork'}
