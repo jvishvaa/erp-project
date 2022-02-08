4dbb2c424968bbcebda5c7adc906294eeef49502
jayesh.solanki1 <jayesh.solanki@orchids.edu.in>
2022-02-08 23:55:00 +0530

    added no filter data

--- src/containers/time-table/index.jsx
+++ src/containers/time-table/index.jsx
@@ -24,7 +24,7 @@ import FilterMobile from './filterMobile/filterMobile';
 import MomentUtils from '@date-io/moment';
 import moment from 'moment';
 import { getPeriodTypes, createPeriod } from './date-and-calander/apis';
-import NoFilterData from 'components/noFilteredData/noFilterData';
+import NoFilterData from '../../components/noFilteredData/noFilterData';
 import clsx from 'clsx';
 import './timetable.scss';
 import Filters from './uppergrade/Filters';
