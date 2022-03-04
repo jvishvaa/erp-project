dc3de3a9eac13564084ee40a2a5cfcbd0f828796
DESKTOP-3PFTMF2\admin <krishna.bhati@orchids.edu.in>
2022-03-04 10:48:34 +0530

    period id on hard referesh on view hw goes undefiened

--- src/containers/academicCalendar/editPeriod/editPeriod.js
+++ src/containers/academicCalendar/editPeriod/editPeriod.js
@@ -224,8 +224,11 @@ const EditPeriod = withRouter(({ history, ...props }) => {
   const handleViewHomeWork = (HW_id) => {
     history.push({
       pathname: `/academic-calendar/view-home-work/${HW_id}`,
-      homeworkId: HW_id,
-      period_id: id,
+      state :{
+        homeworkId: HW_id,
+        period_id: id,
+      }
+      
     });
   };
 
