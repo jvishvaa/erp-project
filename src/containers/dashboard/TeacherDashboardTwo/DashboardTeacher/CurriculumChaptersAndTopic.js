4eb87ca5b6422bf90a3bbc5c6c5cb754ee3bc8cb
chander.mohan <chander.mohan@orchids.edu.in>
2022-03-25 17:59:02 +0530

    handle api for undefined

--- src/containers/dashboard/TeacherDashboardTwo/DashboardTeacher/CurriculumChaptersAndTopic.js
+++ src/containers/dashboard/TeacherDashboardTwo/DashboardTeacher/CurriculumChaptersAndTopic.js
@@ -250,7 +250,8 @@ const ChapterAndTopics = (props) => {
         `${endpoints?.teacherDashboard?.tableData}?grade_id=${props?.history?.location?.state?.gradeId}&section_id=${props?.history?.location?.state?.sectionId}&acad_session_id=${props?.history?.location?.state?.acadSessionId}&subject_id=${props?.history?.location?.state?.subjectId}`,
         {
           headers: {
-            'X-DTS-HOST': 'dev.olvorchidnaigaon.letseduvate.com',
+            // 'X-DTS-HOST': 'dev.olvorchidnaigaon.letseduvate.com',
+            'X-DTS-HOST': window.location.host,
             Authorization: `Bearer ${token}`,
           },
         }
