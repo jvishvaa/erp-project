edb98a9c11e5295adc7ce9aaa4f45486538604c3
chander.mohan <chander.mohan@orchids.edu.in>
2022-03-25 18:06:41 +0530

    hide Todays' classes and Training Report

--- src/containers/dashboard/TeacherDashboardTwo/DashboardTeacher/TeacherDashboard.js
+++ src/containers/dashboard/TeacherDashboardTwo/DashboardTeacher/TeacherDashboard.js
@@ -206,9 +206,9 @@ function TeacherDashboard() {
               Dashboard
             </Typography>
             <Grid container spacing={2}>
-              <Grid item xs={12}>
+              {/* <Grid item xs={12}>
                 <TodayClass branchdetail={branchDetail} />
-              </Grid>
+              </Grid> */}
               <Grid item xs={12}>
                 <Grid container spacing={2}>
                   <Grid item xs={6}>
@@ -229,9 +229,9 @@ function TeacherDashboard() {
                       <Grid item xs={12}>
                         <CurriculumCompletionNew curriculumDetail={curriculumDetail} />
                       </Grid>
-                      <Grid item xs={12}>
+                      {/* <Grid item xs={12}>
                         <TrainingReportNew />
-                      </Grid>
+                      </Grid> */}
                     </Grid>
                   </Grid>
                 </Grid>
