ea133fe477dd17b70d92003756caf386f65224c1
kapil3k <kapil.khatri@orchids.edu.in>
2022-07-26 12:33:16 +0530

    Group part commented

--- src/containers/assessment-central/create-assesment/index.js
+++ src/containers/assessment-central/create-assesment/index.js
@@ -803,7 +803,7 @@ const CreateAssesment = ({
                       </Grid>
                       {selectedQuestionPaper &&(
                         <Grid container alignItems='center' style={{ marginTop: 15 }}>
-                        <Grid
+                        {/* <Grid
                           container
                           alignItems='center'
                           justifyContent='center'
@@ -818,7 +818,7 @@ const CreateAssesment = ({
                             inputProps={{ 'aria-label': 'checkbox with default color' }}
                           />
                           <Typography>Group</Typography>
-                        </Grid>
+                        </Grid> */}
                         {sectionToggle ? (
                           <Grid item xs={12} md={4}>
                             <Autocomplete
