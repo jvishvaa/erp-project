d12f0b4a5de8420a7a165ee10a2845cda69861b6
DESKTOP-3PFTMF2\admin <krishna.bhati@orchids.edu.in>
2022-08-05 16:55:16 +0530

    group selection in create assesment

--- src/containers/assessment-central/create-assesment/index.js
+++ src/containers/assessment-central/create-assesment/index.js
@@ -806,7 +806,7 @@ const CreateAssesment = ({
                       </Grid>
                       {selectedQuestionPaper && (
                         <Grid container alignItems='center' style={{ marginTop: 15 }}>
-                          {/* <Grid
+                          <Grid
                           container
                           alignItems='center'
                           justifyContent='center'
@@ -821,7 +821,7 @@ const CreateAssesment = ({
                             inputProps={{ 'aria-label': 'checkbox with default color' }}
                           />
                           <Typography>Group</Typography>
-                        </Grid> */}
+                        </Grid>
                           {sectionToggle ? (
                             <Grid item xs={12} md={4}>
                               <Autocomplete
