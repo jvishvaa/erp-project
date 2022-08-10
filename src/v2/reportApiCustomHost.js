c542546e203086317b877adfb14f14ca37e22415
Rohan Bharati <rohan.bharati@orchids.edu.in>
2022-08-10 20:17:26 +0530

    Bug Fixes

--- src/v2/reportApiCustomHost.js
+++ src/v2/reportApiCustomHost.js
@@ -2,7 +2,7 @@ if (
   window.location.href.includes('localhost') ||
   window.location.href.includes('ui-revamp1')
 ) {
-  var X_DTS_HOST = 'qa.olvorchidnaigaon.letseduvate.com';
+  var X_DTS_HOST = 'dev.olvorchidnaigaon.letseduvate.com';
 } else {
   var X_DTS_HOST = window.location.host;
 }
