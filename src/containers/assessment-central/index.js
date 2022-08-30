89f60f438ff9aa38cf39f95ea85de41d4838f3e0
DESKTOP-3PFTMF2\admin <krishna.bhati@orchids.edu.in>
2022-08-30 18:29:55 +0530

    conflict resolved

--- src/containers/assessment-central/index.js
+++ src/containers/assessment-central/index.js
@@ -390,7 +390,7 @@ const Assesment = ({ handleColumnSelectedTestChange, handleClose }) => {
     }
     formik.handleSubmit();
     sessionStorage.setItem('createTestData', JSON.stringify(formik?.values))
-    sessionStorage.setItem('dropDownData', JSON.stringify({ branch: branchDropdown, grade: grades, subject: subjects, assesmentTypes: assesmentTypes, section: sectionList, group: groupList ,isSectionToggle : sectionToggle,isSectionToggle : sectionToggle}))
+    sessionStorage.setItem('dropDownData', JSON.stringify({ branch: branchDropdown, grade: grades, subject: subjects, assesmentTypes: assesmentTypes, section: sectionList, group: groupList,isSectionToggle : sectionToggle}))
   };
 
   useEffect(() => {
@@ -409,7 +409,7 @@ const Assesment = ({ handleColumnSelectedTestChange, handleClose }) => {
       setAssesmentTypes(testFilterDropdownList?.assesmentTypes)
       setGroupList(testFilterDropdownList?.group)
       setSectionList(testFilterDropdownList?.section)
-      setSectionToggle(testFilterDropdownList?.sectionToggle)
+      setSectionToggle(testFilterDropdownList?.isSectionToggle)
       history.replace({ state: { dataRestore: false } })
       if (testFilterData?.status?.id) formik.handleSubmit();
     }
