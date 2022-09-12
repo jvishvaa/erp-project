1f55b77325b604dc324a46ed89047d23f705e106
DESKTOP-3PFTMF2\admin <krishna.bhati@orchids.edu.in>
2022-09-12 18:41:08 +0530

    setalert import

--- src/containers/user-management/onboarding-report/onboardingReport.js
+++ src/containers/user-management/onboarding-report/onboardingReport.js
@@ -1,15 +1,18 @@
-import React,{useState} from 'react'
+import React,{useState,useContext} from 'react'
 import Layout from 'containers/Layout'
 import axiosInstance from 'config/axios';
 import endpoints from 'config/endpoints';
 import FileSaver from 'file-saver';
 import { Button } from '@material-ui/core';
 import Loader from 'components/loader/loader';
+import {AlertNotificationContext} from 'context-api/alert-context/alert-state'
 
 
 const OnboardingReport = () => {
 const[loading , setLoading] = useState(false)
 const {user_level} = JSON.parse(localStorage.getItem('userDetails')) || {};
+const { setAlert } = useContext(AlertNotificationContext);
+
 
 
     const downloadExcelFile = () => {
@@ -28,7 +31,7 @@ const {user_level} = JSON.parse(localStorage.getItem('userDetails')) || {};
               });
               FileSaver.saveAs(blob, 'report.xls');
               setLoading(false);
-        }).catch((err) => {
+        }).catch((error) => {
           setAlert('error', error.response.data.message || error.response.data.msg || 'Download Failed !');
         })
       }else{
