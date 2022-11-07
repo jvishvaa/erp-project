8a42a66df9438c9fc86098526a7678cc856f0fcb
Ravi <raviranjan.ptm@gmail.com>
2022-11-07 18:33:40 +0530

    Report card bug fixes

--- src/containers/assessment-central/assesment-report-card/reportCardNewBack/assessmentBackNew.js
+++ src/containers/assessment-central/assesment-report-card/reportCardNewBack/assessmentBackNew.js
@@ -1,19 +1,14 @@
 import React, { useEffect, useState, useContext } from 'react';
 import _ from 'lodash';
 import endpoints from 'v2/config/endpoints';
-import { useSelector } from 'react-redux';
 import axios from 'v2/config/axios';
 
 export default function AssesmentReportBackNew({ reportCardDataNew }) {
   const [pricipalSignData, setPricipalSignData] = useState([]);
 
-  const selectedBranch = useSelector(
-    (state) => state.commonFilterReducer?.selectedBranch
-  );
-
   useEffect(() => {
     fetchPrincipalSignature({
-      branch_id: selectedBranch?.branch?.id,
+      branch_id: reportCardDataNew?.school_info?.branch_id,
     });
   }, [reportCardDataNew]);
 
