967b2d8ed12bbd57f9b68bb8174a22bf5f422e7d
avikdas220797 <avik.das@orchids.edu.in>
2023-01-02 17:18:07 +0530

    erp category subject wise added

--- src/containers/assessment-central/assesment-view/index.js
+++ src/containers/assessment-central/assesment-view/index.js
@@ -166,7 +166,7 @@ if(tabGradeId && (tabSubjectId || erpCategory)){
     if(subject || !erpCategory){
       requestURL += `&subjects=${subject?.value}`
     }
-    if (!subject && erpCategory) {
+    if (!subject && erpCategory?.value != '0') {
       requestURL += `&category=${erpCategory?.value}`; //isErpCentral?.flag ? erpCategory?.central_category_id : erpCategory?.erp_category_id
     }
     handleGetQuestionPapers(newValue, requestURL);
