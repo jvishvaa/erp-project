c8c6c49f4b09f339f1e526bbbc20c9a6e4a9d712
DESKTOP-3PFTMF2\admin <krishna.bhati@orchids.edu.in>
2023-01-05 19:15:31 +0530

    test edit handle

--- src/containers/assessment-central/create-assesment/index.js
+++ src/containers/assessment-central/create-assesment/index.js
@@ -483,10 +483,10 @@ const CreateAssesment = ({
     setSelectedGroupData({});
     setSelectedGroupId('');
     if (value) {
-      const sections = value?.group_section_mapping.map((i) => i?.section_mapping_id);
+      const sections = value[0]?.group_section_mapping?.map((i) => i?.section_mapping_id);
       setGroupSectionMappingId(sections);
-      setSelectedGroupData(value);
-      setSelectedGroupId(value?.id);
+      setSelectedGroupData(value[0]);
+      setSelectedGroupId(value[0]?.id);
     }
   };
 
