29e279411739c6ee659d9416a9d8c519d579a70a
rohan.bharati <rohan.bharati@orchids.edu.in>
2023-01-25 14:34:43 +0530

    Typo fixed

--- src/v2/reportApiCustomHost.js
+++ src/v2/reportApiCustomHost.js
@@ -3,7 +3,7 @@ if (
   window.location.href.includes('ui-revamp1')
 ) {
   var X_DTS_HOST = 'qa.olvorchidnaigaon.letseduvate.com';
-} else if (window.location.href.includes('test')) {
+} else if (window.location.href.includes('test.orchids')) {
   var X_DTS_HOST = 'orchids.letseduvate.com';
 } else {
   var X_DTS_HOST = window.location.host;
