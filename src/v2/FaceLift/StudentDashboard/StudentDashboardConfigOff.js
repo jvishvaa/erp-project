7025a7c4ddef03f56cb1b91388f6e6c31f56ab04
rohan.bharati <rohan.bharati@orchids.edu.in>
2023-01-27 20:19:17 +0530

    Fee Defaulter API Call restricted

--- src/v2/FaceLift/StudentDashboard/StudentDashboardConfigOff.js
+++ src/v2/FaceLift/StudentDashboard/StudentDashboardConfigOff.js
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import Layout from 'containers/Layout';
 import Announcement from './components/Announcement';
 import CalendarCard from './components/Calendar';
 import PendingHomework from './components/PendingHomework';
@@ -12,25 +11,14 @@ import axios from 'v2/config/axios';
 import endpoints from 'v2/config/endpoints';
 import { useHistory } from 'react-router-dom';
 import { UsergroupAddOutlined } from '@ant-design/icons';
-import FeeReminderBanner from '../FeeReminderBanner/FeeReminderBanner';
-import { useSelector } from 'react-redux';
-import ENVCONFIG from 'config/config';
 import StudentAttendance from './components/StudentAttendance';
 
 const StudentDashboardConfigOff = () => {
   const [showDoodle, setShowDoodle] = useState(false);
-  const [isUserBlcoked, setIsUserBlcoked] = useState([]);
   const { first_name, user_level } = JSON.parse(localStorage.getItem('userDetails'));
-  // const [isUserBlcoked,setIsUserBlcoked] = useState([])
-  const [isDefaulter, setIsDefaulter] = useState(false);
   const time = new Date().getHours();
   const history = useHistory();
   const [checkOrigin, setCheckOrigin] = useState(false);
-
-  const userDetails = JSON.parse(localStorage.getItem('userDetails')) || {};
-  const selectedAcademicYear = useSelector((state) => state.commonFilterReducer);
-  const branchId = selectedAcademicYear?.selectedBranch?.branch?.id;
-  const sessionYear = selectedAcademicYear?.selectedBranch?.session_year?.session_year;
 
   useEffect(() => {
     const origin = window.location.origin;
@@ -45,20 +33,8 @@ const StudentDashboardConfigOff = () => {
     }
   }, []);
 
-  const fetchDefault = () => {
-    axios
-      .get(`${endpoints.doodle.fetchDoodle}`)
-      .then((response) => {
-        if (response.data.status_code === 200) {
-          setIsDefaulter(response?.data?.data?.is_defaulter);
-          console.log(response, 'orchids', response?.data?.data?.is_defaulter);
-        }
-      })
-      .catch((error) => console.log(error));
-  };
-
   useEffect(() => {
-    fetchDefault();
+    fetchDoodle();
   }, []);
 
   const fetchDoodle = () => {
@@ -71,21 +47,6 @@ const StudentDashboardConfigOff = () => {
       })
       .catch((error) => message.error('error', error?.message));
   };
-
-  const fetchUserStatus = () => {
-    axios
-      .get(
-        `${ENVCONFIG?.apiGateway?.baseFinanceURL}${endpoints.profile.getPendingFeeStatus}?branch_id=${branchId}&session_year=${sessionYear}&erp_search_term=${userDetails?.erp}`
-      )
-      .then((res) => {
-        setIsUserBlcoked(res.data?.result?.results);
-        fetchDoodle();
-      });
-  };
-
-  useEffect(() => {
-    fetchUserStatus();
-  }, [sessionYear]);
 
   const studentrefer = () => {
     history.push('/studentrefer');
@@ -119,20 +80,13 @@ const StudentDashboardConfigOff = () => {
           ''
         )}
       </div>
-      {isUserBlcoked.length > 0 && isDefaulter ? (
-        <FeeReminderBanner data={isUserBlcoked} />
-      ) : (
-        showDoodle && <Doodle />
-      )}
+      {showDoodle && <Doodle />}
       <div className='row pt-3'>
         <div className='col-md-4 th-custom-col-padding'>
           <TodaysClass />
-          {/* <Blogs /> */}
-          {/* <Discussions /> */}
           <CalendarCard />
         </div>
         <div className='col-md-4 th-custom-col-padding'>
-          {/* <PendingClasswork /> */}
           <StudentAttendance />
           <Assessment />
         </div>
