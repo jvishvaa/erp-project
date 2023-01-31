c643355990df02f09745c9d0e6c5d5d020b4e855
rohan.bharati <rohan.bharati@orchids.edu.in>
2023-01-31 12:24:39 +0530

    Version Added in Footer

--- src/containers/footer/index.jsx
+++ src/containers/footer/index.jsx
@@ -1,20 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Box from '@material-ui/core/Box';
 import Link from '@material-ui/core/Link';
 import Typography from '@material-ui/core/Typography';
 import CallIcon from '@material-ui/icons/Call';
 import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import axios from 'axios';
+import endpoints from 'v2/config/endpoints';
+import { IsV2Checker } from 'v2/isV2Checker';
 const { user_level } = JSON.parse(localStorage.getItem('userDetails')) || {};
 const isStudent = user_level == 13 ? true : false;
-const isFromOrchids = window.location.host.slice(0,7) === 'orchids' ? true : false
+const isFromOrchids = window.location.host.slice(0, 7) === 'orchids' ? true : false;
+
 function TermsAndCondition(props) {
   return (
     <Typography variant='body2' color='textSecondary' align='center'>
+      {props?.version && (
+        <>
+          <span style={{ textDecoration: 'none' }}>{props?.version}&nbsp;</span>|
+        </>
+      )}
       <Link
         style={{ cursor: 'pointer', textDecoration: 'none' }}
         onClick={() => (window.location.pathname = '/terms-condition')}
       >
-        Terms and Conditions&nbsp;
+        &nbsp;Terms and Conditions&nbsp;
       </Link>
       |
       <Link
@@ -40,19 +49,53 @@ function ContactNumber() {
     <Typography variant='body2' color='textSecondary' align='center'>
       <div style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
         <CallIcon />
-        <Link href='tel:8888888999' style={{ marginTop: 5 ,textDecoration:"none"}}>8888888999</Link> &nbsp; &nbsp;
+        <Link href='tel:8888888999' style={{ marginTop: 5, textDecoration: 'none' }}>
+          8888888999
+        </Link>{' '}
+        &nbsp; &nbsp;
         <WhatsAppIcon />
-        <Link href='https://wa.me/918884016161' style={{ marginTop: 5 ,textDecoration:"none"}}>8884016161</Link>
+        <Link
+          href='https://wa.me/918884016161'
+          style={{ marginTop: 5, textDecoration: 'none' }}
+        >
+          8884016161
+        </Link>
       </div>
     </Typography>
   );
 }
+
 const Footer = () => {
+  const isV2 = IsV2Checker();
+  const [version, setVersion] = useState();
+  const fetchVersion = () => {
+    axios
+      .get(`${endpoints.appVersion}`, {
+        headers: {
+          'x-api-key': 'vikash@12345#1231',
+        },
+      })
+      .then((result) => {
+        if (result?.data?.status_code === 200) {
+          if (isV2) {
+            setVersion(result?.data?.result?.v2);
+          } else {
+            setVersion(result?.data?.result?.v1);
+          }
+        }
+      })
+      .catch((error) => {
+        console.error(error?.message);
+      });
+  };
+  useEffect(() => {
+    fetchVersion();
+  }, []);
   return (
     <Box p={2} width='auto'>
       <Copyright />
-      <TermsAndCondition />
-      {(isStudent && isFromOrchids) && <ContactNumber />}
+      <TermsAndCondition version={version} />
+      {isStudent && isFromOrchids && <ContactNumber />}
     </Box>
   );
 };
