f3897351702bc1032a4dc34bbc5862703e69046a
rohan.bharati <rohan.bharati@orchids.edu.in>
2023-02-27 18:20:26 +0530

    Validation made visible

--- src/containers/newBlog/BlogReview.js
+++ src/containers/newBlog/BlogReview.js
@@ -1,90 +1,21 @@
-import React, { useState, useRef, useEffect, useContext, createRef } from 'react';
+import React, { useState, useEffect, createRef } from 'react';
 import { useSelector } from 'react-redux';
 import Layout from 'containers/Layout';
-import {
-  Grid,
-  Typography,
-  TextField,
-  // Button,
-  Box,
-  Divider,
-  Breadcrumbs,
-} from '@material-ui/core';
-import StarsIcon from '@material-ui/icons/Stars';
-import RatingScale from './RatingScale';
 import { X_DTS_HOST } from 'v2/reportApiCustomHost';
-import ArrowBackIcon from '@material-ui/icons/ArrowBack';
-import Autocomplete from '@material-ui/lab/Autocomplete';
 import PropTypes from 'prop-types';
 import PendingReview from './PendingReview';
 import Published from './Published';
-import NotSubmitted from './NotSubmitted';
 import Reviewed from './Reviewed';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import BookmarksIcon from '@material-ui/icons/Bookmarks';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import { useHistory } from 'react-router-dom';
 import Shortlisted from './Shortlisted_1';
 import axiosInstance from 'config/axios';
 import endpoints from 'config/endpoints';
-import { NavigateNext as NavigateNextIcon } from '@material-ui/icons';
 import axios from 'axios';
-import { AlertNotificationContext } from 'context-api/alert-context/alert-state';
-import Loader from 'components/loader/loader';
 import { Breadcrumb, Form, Select, Button, message, Tabs, Drawer } from 'antd';
 import { SearchOutlined } from '@ant-design/icons';
 
-const DEFAULT_RATING = 0;
-
-const useStyles = makeStyles((theme) => ({
-  branch: {
-    fontSize: '12px',
-    marginLeft: '20px',
-    color: 'gray',
-  },
-  home: {
-    marginLeft: '5px',
-    marginTop: '-2px',
-    marginBottom: '15px',
-    fontSize: '15px',
-  },
-  selected1: {
-    background: `${theme.palette.primary.main} !important`,
-    color: 'white !important',
-    borderRadius: '4px',
-  },
-  selected2: {
-    background: `${theme.palette.primary.main} !important`,
-    color: 'white !important',
-    borderRadius: '4px',
-  },
-  tabsFont: {
-    '& .MuiTab-wrapper': {
-      color: 'white',
-      fontWeight: 'bold',
-    },
-  },
-  tabsFont1: {
-    '& .MuiTab-wrapper': {
-      color: 'black',
-      fontWeight: 'bold',
-    },
-  },
-  topic: {
-    marginLeft: '20px',
-    background: '#F1F4F6',
-  },
-  topicName: {
-    color: '#173260',
-  },
-  grade: {
-    color: '#1B689A',
-  },
-}));
-
 const BlogReview = () => {
   const { TabPane } = Tabs;
-  const classes = useStyles();
   const formRef = createRef();
   const { Option } = Select;
   const history = useHistory();
@@ -223,7 +154,6 @@ const BlogReview = () => {
             `${endpoints.newBlog.gradesERP}?session_year=${selectedAcademicYear?.id}&branch_id=${selectedBranch?.branch?.id}&module_id=${moduleId}`
           )
           .then((response) => {
-            // debugger
             setGradeList(response?.data?.data);
             setLoading(false);
           });
@@ -353,16 +283,6 @@ const BlogReview = () => {
                         setFlag={setFlag}
                       />
                     </TabPane>
-                    {/* <TabPane tab='NOT SUBMITTED' key='1'>
-                      <NotSubmitted
-                        selectedBranch={selectedBranch?.branch}
-                        setValue={setValue}
-                        value={value}
-                        selectedGrade={selectedGrade}
-                        flag={flag}
-                        setFlag={setFlag}
-                      />
-                    </TabPane> */}
                     <TabPane tab='REVIEWED' key='2'>
                       <Reviewed
                         selectedBranch={selectedBranch?.branch}
@@ -403,230 +323,6 @@ const BlogReview = () => {
           </div>
         </div>
       </Layout>
-
-      {/* {loading && <Loader />}
-      <Layout>
-        <Grid
-          container
-          direction='row'
-          style={{ paddingLeft: '22px', paddingRight: '10px' }}
-        >
-          <Grid item xs={12} md={6} style={{ marginBottom: 15 }}>
-            <Breadcrumbs
-              style={{ width: '70vw' }}
-              separator={<NavigateNextIcon fontSize='small' style={{ color: 'black' }} />}
-              aria-label='breadcrumb'
-            >
-              <Typography color='textPrimary' style={{ fontSize: '18px' }}>
-                <strong>Activity Management</strong>
-              </Typography>
-              <Typography
-                color='textPrimary'
-                style={{ fontSize: '18px', fontWeight: 'bold' }}
-              >
-                Activity
-              </Typography>
-              <Typography
-                color='textPrimary'
-                style={{ fontSize: '18px', fontWeight: 'bold' }}
-              >
-                {ActivityId?.title}
-              </Typography>
-            </Breadcrumbs>
-          </Grid>
-        </Grid>
-        <div
-          style={{
-            width: '96%',
-            height: 'auto',
-            paddingBottom: '9px',
-            paddingTop: '6px',
-          }}
-        >
-          <div style={{ marginLeft: '22px', marginTop: '9px' }}>
-            <div style={{ display: 'flex' }}>
-              <div style={{ fontSize: '16px', fontWeight: '400' }}>Topic Name: </div>
-              <div
-                style={{
-                  fontSize: '16px',
-                  fontWeight: 'bold',
-                  width: '91%',
-                  paddingLeft: '12px',
-                  fontWeight: '500',
-                  color: '#1C4FA8',
-                }}
-              >
-                {ActivityId?.title}{' '}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div style={{ display: 'flex', padding: '20px' }}>
-          <div style={{ width: '21%', paddingLeft: '42px' }}>
-            <Autocomplete
-              fullWidth
-              limitTags={1}
-              size='small'
-              className='filter-student meeting-form-input'
-              options={gradeList || []}
-              getOptionLabel={(option) => option?.name || ''}
-              filterSelectedOptions
-              value={selectedGrade || []}
-              onChange={(event, value) => {
-                handleGrade(event, value);
-              }}
-              renderInput={(params) => (
-                <TextField
-                  {...params}
-                  required
-                  fullWidth
-                  variant='outlined'
-                  label='Grade'
-                />
-              )}
-            />
-          </div>
-          <div style={{ paddingLeft: '39px' }}>
-            <Button
-              variant='contained'
-              color='primary'
-              style={{ width: '112px', backgroundColor: 'primary' }}
-              onClick={goSearch}
-            >
-              Search
-            </Button>
-          </div>
-          <div style={{ paddingLeft: '39px' }}>
-            <Button
-              variant='contained'
-              color='primary'
-              onClick={goBack}
-              style={{ width: '112px' }}
-            >
-              Back
-            </Button>
-          </div>
-        </div>
-
-        <div style={{ display: 'flex', justifyContent: 'space-between' }}>
-          <div>
-            <Grid container style={{ paddingLeft: '21px' }}>
-              <Grid item md={12} xs={12} className={classes.tabStatic}>
-                <Tabs
-                  onChange={handleChange}
-                  textColor='primary'
-                  indicatorColor='primary'
-                  value={value}
-                >
-                  <Tab
-                    label='Pending Review'
-                    classes={{
-                      selected: classes.selected2,
-                    }}
-                    className={value === 0 ? classes.tabsFont : classes.tabsFont1}
-                  />
-                  <Tab
-                    label='Not Submitted'
-                    classes={{
-                      selected: classes.selected1,
-                    }}
-                    className={value === 1 ? classes.tabsFont : classes.tabsFont1}
-                  />
-                  <Tab
-                    label='Reviewed'
-                    classes={{
-                      selected: classes.selected1,
-                    }}
-                    className={value === 2 ? classes.tabsFont : classes.tabsFont1}
-                  />
-                  <Tab
-                    label='Shortlisted'
-                    classes={{
-                      selected: classes.selected1,
-                    }}
-                    className={value === 3 ? classes.tabsFont : classes.tabsFont1}
-                  />
-
-                  {dataes?.user_level == '11' || dataes?.user_level == '8' ? (
-                    ''
-                  ) : (
-                    <Tab
-                      label='Published'
-                      classes={{
-                        selected: classes.selected1,
-                      }}
-                      className={value === 4 ? classes.tabsFont : classes.tabsFont1}
-                    />
-                  )}
-                </Tabs>
-              </Grid>
-            </Grid>
-          </div>
-          <div>
-            {value == 2 && (
-              <div style={{ marginRight: '49px' }}>
-                <BookmarksIcon style={{ color: 'gray' }} /> Shortlisted
-              </div>
-            )}
-          </div>
-        </div>
-        {value == 0 && (
-          <PendingReview
-            selectedBranch={selectedBranch?.branch}
-            setValue={setValue}
-            value={value}
-            handleChange={handleChange}
-            selectedGrade={selectedGrade}
-            flag={flag}
-            setFlag={setFlag}
-          />
-        )}
-        {value == 1 && (
-          <NotSubmitted
-            selectedBranch={selectedBranch?.branch}
-            setValue={setValue}
-            value={value}
-            handleChange={handleChange}
-            selectedGrade={selectedGrade}
-            flag={flag}
-            setFlag={setFlag}
-          />
-        )}
-        {value == 2 && (
-          <Reviewed
-            selectedBranch={selectedBranch?.branch}
-            setValue={setValue}
-            value={value}
-            handleChange={handleChange}
-            selectedGrade={selectedGrade}
-            flag={flag}
-            setFlag={setFlag}
-          />
-        )}
-        {value == 3 && (
-          <Shortlisted
-            selectedBranch={selectedBranch?.branch}
-            setValue={setValue}
-            value={value}
-            handleChange={handleChange}
-            selectedGrade={selectedGrade}
-            flag={flag}
-            setFlag={setFlag}
-          />
-        )}
-        {value == 4 && (
-          <Published
-            selectedBranch={selectedBranch?.branch}
-            setValue={setValue}
-            value={value}
-            handleChange={handleChange}
-            selectedGrade={selectedGrade}
-            flag={flag}
-            setFlag={setFlag}
-          />
-        )}
-      </Layout> */}
     </div>
   );
 };
