07c571a5770f7ce26516e042f753f1470e3f8920
avikdas220797 <avik.das@orchids.edu.in>
2023-05-05 16:01:41 +0530

    mcollege and dps activated

--- src/components/bulk-upload/index.js
+++ src/components/bulk-upload/index.js
@@ -180,7 +180,7 @@ const BulkUpload = ({ onUploadSuccess }) => {
 
   const isOrchids =
   window.location.host.split('.')[0] === 'orchids' ||
-  window.location.host.split('.')[0] === 'qa' || window.location.host.split('.')[0] === 'localhost:3001'
+  window.location.host.split('.')[0] === 'qa' || window.location.host.split('.')[0] === 'localhost:3000' || window.location.host.split('.')[0] === 'mcollege' || window.location.host.split('.')[0] === 'dps'
     ? true
     : false;
 
--- src/containers/user-management/edit-user.js
+++ src/containers/user-management/edit-user.js
@@ -55,7 +55,7 @@ class EditUser extends Component {
       userLevel: '',
       designation: '',
       isOrchids : window.location.host.split('.')[0] === 'orchids' ||
-      window.location.host.split('.')[0] === 'qa' || window.location.host.split('.')[0] === 'localhost:3000'
+      window.location.host.split('.')[0] === 'qa' || window.location.host.split('.')[0] === 'localhost:3000' || window.location.host.split('.')[0] === 'mcollege' || window.location.host.split('.')[0] === 'dps'
         ? true
         : false,
     };
