6351ec684f24c713d6db87ebe9b4755d3a7bfa6e
Gautam Kumar <gautam.kr@orchids.edu.in>
2023-09-29 16:30:02 +0530

    diary name issue

--- src/v2/FaceLift/Diary/DiaryNew/Diary-Card/DailyDiaryCard.js
+++ src/v2/FaceLift/Diary/DiaryNew/Diary-Card/DailyDiaryCard.js
@@ -1321,11 +1321,20 @@ const DailyDairyCard = ({ diary, fetchDiaryList, subject, isStudentDiary }) => {
                 >
                   <div className='th-16' style={{ height: 120, overflowY: 'auto' }}>
                     {diary?.documents?.map((each) => {
-                      const fullName = each?.split('_.')[0]?.split('/');
-                      // const fileName = fullName.split('.')[fullName?.split('.').length - 2];
-                      const fileName = fullName[fullName.length - 1];
-                      // const extension = fullName.split('.')[fullName?.split('.').length - 1];
-                      const extension = each.split('_.')[each?.split('_.').length - 1];
+                      // const fullName = each?.split('.')[0]?.split('/');
+                      // // const fileName = fullName.split('.')[fullName?.split('.').length - 2];
+                      // const fileName = fullName[fullName.length - 1];
+                      // // const extension = fullName.split('.')[fullName?.split('.').length - 1];
+                      // const extension = each.split('_.')[each?.split('_.').length - 1];
+
+                      const fullName = each?.split('_')[each?.split('_').length - 1];
+                      const fileName =
+                        fullName.split('.')[fullName?.split('.').length - 2];
+                      const extension =
+                        fullName.split('.')[fullName?.split('.').length - 1];
+
+                      const fileName2 = each?.split('/')[each?.split('/').length - 1]
+
                       return (
                         <div
                           className='row mt-2 py-2 align-items-center th-bg-grey'
@@ -1362,9 +1371,9 @@ const DailyDairyCard = ({ diary, fetchDiaryList, subject, isStudentDiary }) => {
                                       overflow: 'hidden',
                                       textOverflow: 'ellipsis',
                                     }}
-                                    title={`${fileName}.${extension}`}
+                                    title={`${fileName2}.${extension}`}
                                   >
-                                    {fileName}
+                                    {fileName2}
                                   </p>
                                 </div>
                                 <div className='col-2'>
