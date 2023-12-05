38ef708c799c1d6cca08df10f0aa3f0fc4960bf4
Admin <admin@Admins-MacBook-Pro.local>
2023-12-05 18:39:44 +0530

    fixed endpoint

--- src/v2/FaceLift/Timetable/PrincipalTimeTable/SetTimeTable.js
+++ src/v2/FaceLift/Timetable/PrincipalTimeTable/SetTimeTable.js
@@ -362,7 +362,7 @@ const SetTimeTable = ({ showTab }) => {
     setCreatePeriodLoading(true);
     axios
       .post(
-        `${endpoints?.timeTableNewFlow?.periodSlots}g/${selectedSlotData?.id}/`,
+        `${endpoints?.timeTableNewFlow?.periodSlots}/${selectedSlotData?.id}/`,
         currentSlotPeriods
       )
       .then((res) => {
