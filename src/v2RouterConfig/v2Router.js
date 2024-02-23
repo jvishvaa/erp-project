f83c6b29573cbfc388c11a1d40c6dee93fad399d
Rohan Bharati <rohan.bharati@orchids.edu.in>
2024-02-23 12:42:20 +0530

    enable android management

--- src/v2RouterConfig/v2Router.js
+++ src/v2RouterConfig/v2Router.js
@@ -78,9 +78,9 @@ import SignatureUploadv2 from 'v2/FaceLift/MasterManagement/signature-upload/sig
 import { IsOrchidsChecker } from 'v2/isOrchidsChecker';
 import EditReportConfig from 'containers/assessment-central/ReportCardConfig/EditReportConfig';
 import CreatePeReportConfig from 'containers/assessment-central/ReportCardConfig/PhysicalEducation/CreatePeReportConfig';
-// import EnterPrises from 'v2/FaceLift/AndroidManagement/Enterprises';
-// import Devices from 'v2/FaceLift/AndroidManagement/Devices';
-// import Policies from 'v2/FaceLift/AndroidManagement/Policies';
+import EnterPrises from 'v2/FaceLift/AndroidManagement/Enterprises';
+import Devices from 'v2/FaceLift/AndroidManagement/Devices';
+import Policies from 'v2/FaceLift/AndroidManagement/Policies';
 
 const V2Router = () => {
   useEffect(() => {
@@ -389,7 +389,7 @@ const V2Router = () => {
                           {({ match }) => <SignatureUploadv2 match={match} />}
                         </Route>
                         ,
-                        {/* <Route path='/enterprise-management/:enterPriseName/:enterPriseId/devices'>
+                        <Route path='/enterprise-management/:enterPriseName/:enterPriseId/devices'>
                           {({ match }) => <Devices match={match} />}
                         </Route>
                         <Route path='/enterprise-management/:enterPriseName/:enterPriseId/policies'>
@@ -397,7 +397,7 @@ const V2Router = () => {
                         </Route>
                         <Route path='/enterprise-management/enterprises'>
                           {({ match }) => <EnterPrises match={match} />}
-                        </Route> */}
+                        </Route>
                         {/* v1 router */}
                         {V1Router?.map((item) => {
                           return item;
