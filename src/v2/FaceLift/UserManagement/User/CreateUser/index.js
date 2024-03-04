0e6c618d74115bbd6f4b726804be4a1ce235e945
Rohan Bharati <rohan.bharati@orchids.edu.in>
2024-03-04 10:39:47 +0530

    device expiry removed

--- src/v2/FaceLift/UserManagement/User/CreateUser/index.js
+++ src/v2/FaceLift/UserManagement/User/CreateUser/index.js
@@ -351,7 +351,6 @@ const CreateUser = () => {
             aadhaar: user?.aadhaar,
           },
           user_level: user?.user_level,
-          device_expiry: user?.device_expiry ?? '',
           designation: user?.designation,
           siblings: user?.siblings,
         };
@@ -416,7 +415,6 @@ const CreateUser = () => {
           user_level: transformedSchoolDetails?.user_level,
           designation: transformedSchoolDetails?.designation?.id,
           academic_year: academicYearObj[0]?.session_year,
-          device_expiry: transformedSchoolDetails?.device_expiry ? moment(transformedSchoolDetails?.device_expiry) : '',
           branch: transformedUser?.branch?.pop()?.map((e) => e.id),
           grade: gradeObj?.map((e) => e.grade_name),
           section: sectionObj?.map((e) => e.section_name),
@@ -747,11 +745,6 @@ const CreateUser = () => {
     );
     formData.append('age', studentFormValues?.age ?? '');
     formData.append('birth_place', studentFormValues?.birth_place ?? '');
-    formData.append(
-      'device_expiry',
-      moment(schoolFormValues?.device_expiry).format('YYYY-MM-DD') ?? ''
-    );
-
     formData.append('old_school_name', studentFormValues?.old_school_name ?? '');
     formData.append('special_needs', studentFormValues?.special_needs ?? '');
     formData.append('medical_info', studentFormValues?.medical_info ?? '');
