e749b705ade2422de30ad699a231c232b5555642
Chakresh <chakresh@orchids.edu.in>
2024-03-14 15:20:41 +0530

    config revert

--- src/v2/config/config.js
+++ src/v2/config/config.js
@@ -90,8 +90,7 @@ const stage = {
     FINANCE_BUCKET: 'https://storage.googleapis.com/letseduvate/prod/media',
   },
   apiGateway: {
-    // baseURL: `${window.location.origin}/qbox`,
-    baseURL: `https://orchids-stage.stage-vm.letseduvate.com/qbox`,
+    baseURL: `${window.location.origin}/qbox`,
     baseURLMPQ: 'https://stage.mpquiz.letseduvate.com',
     baseUdaan: 'https://udanta.dev-k8.letseduvate.com/qbox',
     // baseURLCentral: 'https://mgmt-stage.letseduvate.com/qbox',
@@ -150,7 +149,7 @@ const DEV = 'DEV';
 const QA = 'QA';
 const STAGE = 'STAGE';
 const env = { [PROD]: prod, [DEV]: dev, [QA]: qa, [STAGE]: stage };
-const config = env[process.env.REACT_APP_UI_ENV] || stage;
+const config = env[process.env.REACT_APP_UI_ENV] || qa;
 
 export default {
   // Add common config values here
