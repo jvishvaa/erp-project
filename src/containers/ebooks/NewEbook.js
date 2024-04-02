106a5e52045947d44a0860056e9501694bbd5c7f
Girish-Adapa <girishsaiadapa1999@gmail.com>
2024-04-02 15:38:24 +0530

    ibook-config-task-changes

--- src/containers/ebooks/NewEbook.js
+++ src/containers/ebooks/NewEbook.js
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import Layout from 'containers/Layout';
-import { Breadcrumb, Tabs, Select } from 'antd';
+import { Breadcrumb, Tabs, Select, message } from 'antd';
 import EbookView from './EbooknewView';
 import FeeReminder from 'v2/FaceLift/FeeReminder/FeeReminder';
 import EbookViewStudent from './ebookViewStudent';
+import Loading from 'components/loader/loader';
 import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import endpoints from 'v2/config/endpoints';
 const { TabPane } = Tabs;
 
 const NewEbookView = (props) => {
@@ -12,6 +15,28 @@ const NewEbookView = (props) => {
   const { user_level } = JSON.parse(localStorage.getItem('userDetails')) || {};
   const [showTab, setShowTab] = useState('1');
   const [changeRecent, setChangeRecent] = useState(false);
+  const [ibookConfig, setIbookConfig] = useState(false);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    // hit ibook config api and based on api response block ibooks
+    fetchIbookConfig();
+  }, []);
+  const fetchIbookConfig = () => {
+    setLoading(true);
+    axios
+      .get(`${endpoints.newibook.ibookConfig}`)
+      .then((response) => {
+        if (response.data.status_code === 200) {
+          setIbookConfig(response?.data?.result);
+        }
+      })
+      .catch((error) => {
+        message.error('error', error?.message);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
 
   const onChangeTab = (e) => {
     setShowTab(e);
@@ -28,6 +53,7 @@ const NewEbookView = (props) => {
   return (
     <React.Fragment>
       <Layout>
+        {loading ? <Loading message='Loading...' /> : null}
         <div className='row py-3 px-2'>
           <div className='col-md-6 th-bg-grey' style={{ zIndex: 2 }}>
             <Breadcrumb separator='>'>
@@ -54,13 +80,15 @@ const NewEbookView = (props) => {
                       <EbookView showTab={showTab} changeRecent={changeRecent} />
                     )}
                   </TabPane>
-                  <TabPane tab='IBOOK' key='2'>
-                    {user_level == 13 ? (
-                      <EbookViewStudent showTab={showTab} changeRecent={changeRecent} />
-                    ) : (
-                      <EbookView showTab={showTab} changeRecent={changeRecent} />
-                    )}
-                  </TabPane>
+                  {ibookConfig === "True" && (
+                    <TabPane tab='IBOOK' key='2'>
+                      {user_level == 13 ? (
+                        <EbookViewStudent showTab={showTab} changeRecent={changeRecent} />
+                      ) : (
+                        <EbookView showTab={showTab} changeRecent={changeRecent} />
+                      )}
+                    </TabPane>
+                  )}
                 </Tabs>
               </div>
             </div>
