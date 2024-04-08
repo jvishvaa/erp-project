e54fb346d06132043c030cc2f5a036a261a3e272
avik <avik.das@orchids.edu.in>
2024-04-08 14:51:54 +0530

    added validation for erp update

--- src/v2/config/config.js
+++ src/v2/config/config.js
@@ -94,7 +94,8 @@ const stage = {
     FINANCE_BUCKET: 'https://storage.googleapis.com/letseduvate/prod/media',
   },
   apiGateway: {
-    baseURL: `${window.location.origin}/qbox`,
+    // baseURL: `${window.location.origin}/qbox`,
+    baseURL: `https://orchids-stage.stage-vm.letseduvate.com/qbox`,
     baseURLMPQ: 'https://stage.mpquiz.letseduvate.com',
     baseUdaan: 'https://udanta.dev-k8.letseduvate.com/qbox',
     // baseURLCentral: 'https://mgmt-stage.letseduvate.com/qbox',
@@ -153,7 +154,7 @@ const DEV = 'DEV';
 const QA = 'QA';
 const STAGE = 'STAGE';
 const env = { [PROD]: prod, [DEV]: dev, [QA]: qa, [STAGE]: stage };
-const config = env[process.env.REACT_APP_UI_ENV] || qa;
+const config = env[process.env.REACT_APP_UI_ENV] || stage;
 
 export default {
   // Add common config values here
